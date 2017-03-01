b44727793aa411001d16484ef12600859ec0d1b4
Michał Galas <michal@webspirit.ie>
2017-03-01 11:20:56 +0100

    travis config

---
+++ server/config2.js
@@ -0,0 +1,11 @@
+module.exports = {
+  db: 'mongodb://@127.0.0.1:27017/food',
+  jwt: {
+    secret: '9qRHW@tgh:A<@cuR4CpLG7p#/&w&?'
+  },
+  slack: {
+    CLIENT_ID: '15385086278.134021295030',
+    CLIENT_SECRET: '0af38705652d27307c107e3b78b49d1c',
+    callbackUrl: 'http://192.168.1.12/auth/slack/callback'
+  }
+}
